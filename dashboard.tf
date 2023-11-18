3ab76caf3a9c1237b754e73511b4a82c1bcaa3f7
vytre <vegaytre@gmail.com>
2023-11-18 05:26:55 +0000

    Updated Dashboard

--- dashboard.tf
+++ dashboard.tf
@@ -13,7 +13,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "metrics": [
           [
             "${var.kandidat}",
-            "account_count.value"
+            "image_count.value"
           ]
         ],
         "period": 300,

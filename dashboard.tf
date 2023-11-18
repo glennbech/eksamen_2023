0e4db48534d81bbfbd5aa66024b54b237df4b4f2
vytre <vegaytre@gmail.com>
2023-11-18 05:19:39 +0000

    added stuff

---
+++ dashboard.tf
@@ -0,0 +1,28 @@
+resource "aws_cloudwatch_dashboard" "main" {
+  dashboard_name = var.kandidat
+  dashboard_body = <<THEREBEDRAGONS
+{
+  "widgets": [
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.kandidat}",
+            "account_count.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Some thing"
+      }
+    }
+  ]
+}
+THEREBEDRAGONS
+}

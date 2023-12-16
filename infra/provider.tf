5c3aefba2cb268c0902a750d997f536ae272334f
glennbech <glenn.bech@gmail.com>
2023-12-16 15:10:14 +0100

    try master go

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "kandidat-2020/apprunner-actions.state"
+    key    = "kandidat-2020s/apprunner-actions.state"
     region = "eu-north-1"
 }
 }
